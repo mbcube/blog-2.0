d3e0521017335bf7220342abeb6b3d7ca9b523cf
mouad bouras <mouad@mbcubeconsulting.ca>
2026-03-17 22:58:28 -0400

    update logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,69 +23,35 @@ const navItems = {
 function Logo() {
   return (
     <Link aria-label="Mouad Bouras" href="/">
-      <svg
-        width="50"
-        height="43"
-        viewBox="0 0 500 434"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g clipPath="url(#clip0_1_2)">
-          <path
-            d="M69.6714 119.283L75.5196 109.146L131.746 11.8H368.254L418.339 98.6204L428.571 92.8754L374.972 0H125.019L59.5423 113.435L64.5121 116.355L69.6714 119.283Z"
-            fill="white"
-          />
-          <path
-            d="M434.807 103.59L429.743 106.519L424.575 109.439L430.328 119.283L486.452 216.534L368.254 421.372H243.958V433.06H374.972L500 216.534L434.807 103.59Z"
-            fill="white"
-          />
-          <path
-            d="M232.063 421.372H131.746L13.5398 216.534L63.5303 129.998L53.4013 124.15L0 216.534L125.019 433.06H232.063V421.372Z"
-            fill="white"
-          />
-          <path
-            d="M250.047 371.476L252.683 369.917V383.457L250.047 385.024L244.104 381.614L104.167 300.822V153.779L115.864 160.601V294.001L244.104 368.065L250.047 371.476Z"
-            fill="white"
-          />
-          <path
-            d="M377.513 121.721L365.618 128.439L250.047 61.6873L121.807 135.752H121.712L115.864 139.162V145.984L104.167 139.257V132.341L110.015 129.025L250.047 48.1474L377.513 121.721Z"
-            fill="white"
-          />
-          <path
-            d="M395.833 132.341V300.822L267.196 375.085V361.536L384.136 294.001V139.162L378.383 135.846L378.193 135.752L387.056 130.584V130.678L389.985 128.93H390.079L390.174 129.025L395.833 132.341Z"
-            fill="white"
-          />
-          <path
-            d="M392.904 140.816L384.136 145.881L353.241 163.718L255.895 219.945L249.952 223.356L244.104 219.945L146.655 163.718L130.385 154.365L115.863 145.881L107.095 140.816L112.944 130.678V130.584L121.712 135.752H121.807L152.504 153.486L249.952 209.807L347.401 153.486L358.116 147.25L378.193 135.752L387.056 130.584V130.678L387.159 130.782L392.904 140.816Z"
-            fill="white"
-          />
-          <path
-            d="M255.844 216.594H244.104V378.211H255.844V216.594Z"
-            fill="white"
-          />
-          <path
-            d="M255.878 341.528H244.139V427.22H255.878V341.528Z"
-            fill="white"
-          />
-          <path
-            d="M435.393 116.355L430.328 119.283L395.833 139.162L392.904 140.816L384.136 145.881L353.241 163.718L347.401 153.486L358.116 147.25L378.193 135.752L387.056 130.678L389.985 128.93H390.079L390.174 129.025L424.575 109.438L429.743 106.519L435.393 116.355Z"
-            fill="white"
-          />
-          <path
-            d="M152.504 153.486L146.655 163.719L130.385 154.365L115.864 145.984L104.167 139.257L69.6714 119.283L64.5122 116.355L70.4552 106.226L75.5197 109.146L110.015 129.025L112.944 130.679L121.712 135.752H121.807L152.504 153.486Z"
-            fill="white"
-          />
-          <path
-            d="M390.174 129.025L387.16 130.782L378.383 135.846L358.211 147.345L358.116 147.25L378.193 135.752L387.056 130.584V130.679L389.985 128.93H390.079L390.174 129.025Z"
-            fill="white"
-          />
-        </g>
-        <defs>
-          <clipPath id="clip0_1_2">
-            <rect width="500" height="433.06" fill="white" />
-          </clipPath>
-        </defs>
-      </svg>
+<svg width="50" height="50" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <g id="M">
+        <path id="Rectangle" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 72 356 L 110 378.454559 L 110 166.136353 L 72 143.681824 Z"/>
+        <path id="path1" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 160 346 L 211 286.840027 L 208 222 L 160 277 Z"/>
+        <path id="path2" fill="#004d81" fill-rule="evenodd" stroke="none" d="M 141.013489 396.006714 L 160.02063 405.993134 L 159.848694 371.599915 L 140.708679 335.020874 Z"/>
+        <path id="path3" fill="#004d81" fill-rule="evenodd" stroke="none" d="M 159 405.5 L 180 418 L 180 349 L 159 370.5 Z"/>
+        <path id="path4" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 205 287 L 212 287 L 212 224 L 208 222 Z"/>
+        <path id="path5" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 106.852112 234.446777 L 160.458679 346.117554 L 161 275 L 114 169 Z"/>
+        <path id="path6" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 104 212 L 115 219 L 115 169 L 104 162 Z"/>
+        <path id="Rectangle-copy-3" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 211 436 L 250 458.454559 L 250 246.136353 L 211 223.681824 Z"/>
+        <path id="Triangle" fill="#004d81" fill-rule="evenodd" stroke="none" d="M 135.530884 180.288635 L 162.488892 241.927734 L 188.703979 211.025757 Z"/>
+    </g>
+    <g id="B">
+        <path id="path7" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 297 268.836365 L 438 187.160706 L 438 143.744019 L 297 225.419678 Z"/>
+        <path id="path8" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 297 436.75 L 438 355.5 L 438 312.5 L 297 393.75 Z"/>
+        <path id="path9" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 295 352.593506 L 385 301.406494 L 385 259.406494 L 295 310.593506 Z"/>
+        <path id="path10" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 384 300.022186 L 438 329 L 438 288.977814 L 384 260 Z"/>
+        <path id="path11" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 402.999023 280.017517 L 438.019043 299.982483 L 438.000977 281.982483 L 402.980957 262.017517 Z"/>
+        <path id="path12" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 400 337 L 438 337 L 438 297 L 400 297 Z"/>
+        <path id="path13" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 404.479736 298 L 438 234.877228 L 408.520264 214 L 375 277.122772 Z"/>
+        <path id="path14" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 400 234 L 438 234 L 438 181 L 400 181 Z"/>
+        <path id="Rectangle-copy-4" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 260 458.696503 L 299 436.028656 L 299 223.931213 L 260 246.59906 Z"/>
+    </g>
+    <g id="3-bands">
+        <path id="path15" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 215.007996 213.030457 L 254.937988 236.273804 L 431.953247 133.614807 L 392.023193 110.37146 Z"/>
+        <path id="Rectangle-copy" fill="#004d81" fill-rule="evenodd" stroke="none" d="M 146.983261 172.677765 L 187.558655 196.773071 L 364.953247 94.614807 L 324.377808 70.519501 Z"/>
+        <path id="Rectangle-copy-2" fill="#ffffff" fill-rule="evenodd" stroke="none" d="M 78.464584 133.313141 L 118.736832 157.00827 L 295.953247 54.614807 L 255.680939 30.919678 Z"/>
+    </g>
+</svg>
     </Link>
   );
 }
